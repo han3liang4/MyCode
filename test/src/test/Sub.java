069b337d486990ea8bcf2de6e4517153afedf142
han3liang4 <han3liang4@163.com>
2014-05-08 15:13:35 +0800

    测试

---
+++ test/src/test/Sub.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Sub extends Base {
+	public static void main(String[] args) {
+		Sub a=new Sub();
+		Base b=new Base();
+		System.out.println(a.a);
+		Base.a=6;
+		System.out.println(b.a);
+	}
+}
